9f25493bcc9654068e1be005cbe4315992bdc7c1
DengJinlong <dengjinlong@pingan.com.cn>
2015-12-21 19:13:23 +0800

    add link.sh

---
+++ link.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+ln -s ~/spf13-copy-vim/.vimrc ~/.vimrc
+ln -s ~/spf13-copy-vim/.vimrc.local ~/.vimrc.local
+ln -s ~/spf13-copy-vim/.vimrc.bundles ~/.vimrc.bundles
+ln -s ~/spf13-copy-vim/.vimrc.bundles.local ~/.vimrc.bundles.local
+ln -s ~/spf13-copy-vim/.vimrc.bundles.default ~/.vimrc.bundles.default
+ln -s ~/spf13-copy-vim/.vimrc.before ~/.vimrc.before
+ln -s ~/spf13-copy-vim/.vimrc.before.local ~/.vimrc.before.local
+ln -s ~/spf13-copy-vim/.gvimrc ~/.gvimrc
+ln -s ~/spf13-copy-vim/vim ~/.vim
