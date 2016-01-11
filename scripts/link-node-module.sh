6b5ef2c5d339bee36ddbf7d4d6e40260d93d4090
DengJinlong <dengjinlong@pingan.com.cn>
2016-01-11 19:35:20 +0800

    link script

---
+++ scripts/link-node-module.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if [ -z $RNNodeModulesPATH ]
+then
+    echo "Usage: add \"export RNNodeModulesPATH=(your node modules path)\" to .zshrc.local"
+    exit 0
+fi
+
+ln -s $RNNodeModulesPATH `pwd`/node_modules"
